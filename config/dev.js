5632d276712a9e1bb3edca1af6f650c3a4dbf572
shalo serge <sergershalowilson@gmail.com>
2018-09-17 03:02:21 +0800

    first commit

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "635480613070-cnn16e4qpo81s0iej8qbinntgbdlcav9.apps.googleusercontent.com",
+  googleClientSecret: "21kcDELu2X6wl0d7YxMhghkq",
+  mongoURI: "mongodb://root:Louis1951@ds255262.mlab.com:55262/emaily1",
+  cookieKey:
+    "2232dwerwrwer234323rweqwfsadfqwer21321rwqfsadrq2rqfwfsrwqerfqwdsgsfghbgyu"
+};
---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID:
+    "635480613070-cnn16e4qpo81s0iej8qbinntgbdlcav9.apps.googleusercontent.com",
+  googleClientSecret: "21kcDELu2X6wl0d7YxMhghkq",
+  mongoURI: "mongodb://root:Louis1951@ds255262.mlab.com:55262/emaily1",
+  cookieKey:
+    "2232dwerwrwer234323rweqwfsadfqwer21321rwqfsadrq2rqfwfsrwqerfqwdsgsfghbgyu",
+  stripePublishableKey: "pk_test_HR6sgMddOlVwJ7AwqvR8Wu0e",
+  stripeSecretKey: "sk_test_v7ZVDHiaLp9PXgOqQ65c678g"
+};
